65a2e030f1314aed4f80225133b160d334a652ca
Christopher Ventura <99927387+venturaChris@users.noreply.github.com>
2023-03-09 10:43:06 -0300

    Update script.js
    new

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let personagem = document.querySelector ("#personagem")
 let quadrado = document.querySelector ("#quadrado")
 
-function pular (){
+function start(){
+    var myDiv = document.getElementById("game");
+    myDiv.innerHTML = " <div id='personagem'></div> <div id='quadrado'></div>";
+};
+function pular(){
+    let personagem = document.querySelector ("#personagem")
+    let quadrado = document.querySelector ("#quadrado")
     if(personagem.classList != "animar"){
         personagem.classList.add("animar")
-    }
+    };
     
     setTimeout(function(){
         personagem.classList.remove("animar") 
-    },500)  
-}
+    },800)  
+
     var testarcolisao = setInterval (function(){
 
         var topopersonagem = parseInt(
@@ -18,10 +24,10 @@ function pular (){
         var esquerdaquadrado = parseInt(
             window.getComputedStyle(quadrado).     getPropertyValue("left"))
 
-        if (esquerdaquadrado < 20 && esquerdaquadrado > 0 && topopersonagem >= 130) {
+        if (esquerdaquadrado < 20 && esquerdaquadrado > 0 && topopersonagem >= 120) {
             quadrado.style.animation = "none"
             quadrado.style.display = "none"
             alert('voce perdeu, recarregue a pagina')
         }
 
-    }, 10)
+    }, 10);
