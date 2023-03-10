460bf654510cbf40cc2ffd81be46fbfc4ef5d6f8
venturachris <venturachris.cv@gmail.com>
2023-03-09 23:51:12 -0300

    NEW

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-let personagem = document.querySelector ("#personagem")
-let quadrado = document.querySelector ("#quadrado")
+let personagem = document.querySelector("#personagem");
+let quadrado = document.querySelector("#quadrado");
 
 function start(){
     var myDiv = document.getElementById("game");
     myDiv.innerHTML = " <div id='personagem'></div> <div id='quadrado'></div>";
 };
 function pular(){
-    let personagem = document.querySelector ("#personagem")
-    let quadrado = document.querySelector ("#quadrado")
+    let personagem = document.querySelector("#personagem");
+    let quadrado = document.querySelector("#quadrado");
     if(personagem.classList != "animar"){
-        personagem.classList.add("animar")
+        personagem.classList.add("animar");
     };
     
     setTimeout(function(){
-        personagem.classList.remove("animar") 
-    },800)  
+        personagem.classList.remove("animar"); 
+    },800);  
 
     var testarcolisao = setInterval (function(){
 
-        var topopersonagem = parseInt(
-            window.getComputedStyle(personagem).getPropertyValue("top")
-            )
-        var esquerdaquadrado = parseInt(
-            window.getComputedStyle(quadrado).     getPropertyValue("left"))
+        var topopersonagem = parseInt(window.getComputedStyle(personagem).getPropertyValue("top"))
+        var esquerdaquadrado = parseInt(window.getComputedStyle(quadrado).getPropertyValue("left"))
 
         if (esquerdaquadrado < 20 && esquerdaquadrado > 0 && topopersonagem >= 120) {
-            quadrado.style.animation = "none"
-            quadrado.style.display = "none"
-            alert('voce perdeu, recarregue a pagina')
+            quadrado.style.animation = "none";
+            quadrado.style.display = "none";
+            alert('voce perdeu, recarregue a pagina');
         }
 
     }, 10);
+};
+
